07e22503c306dbce5ade86393fbf48ed4a423203
Vinit Kumar <mail@vinitkumar.me>
2019-02-19 12:04:27 +0530

    all our code is prettier now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { lazy, Suspense, PureComponent, useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React, {lazy, Suspense, PureComponent, useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import fetchData from './services/api';
 import Loader from './Loader';
@@ -15,59 +15,82 @@ function App() {
   function handleRefresh(event) {
     setloading(true);
     const data = fetchData();
-    data.then((gists) => {
+    data.then(gists => {
       setgistList(gists);
       setloading(false);
     });
   }
 
   useEffect(() => {
-    console.log('I am hooks');
     const data = fetchData();
-    data.then((gists) => {
+    data.then(gists => {
       setgistList(gists);
       setloading(false);
     });
     return () => {
       // do cleanup here
     };
+    // this {} is required to simulate something like ComponentDidMount and ComponentDidUnmount
+    // in hooks. Else, there will be hits to the API on infinite loop.
   }, {});
 
   return (
-      <Router>
-        <div className="container-fluid">
-          <h1><Link to="/">Gist Dashboard</Link>
-            <a className="refresh" title="Refresh" onClick={(e) => handleRefresh(e)}>
-              {!loading && <i className="fa fa-refresh"></i> }
-              {loading && <Loader />}
-            </a>
-          </h1>
-          { error && <div className="alert alert-danger" role="alert">error: {error} </div>}
-          <div className="row">
-            <div className="col-4 sidebar">
-              { !error &&
-                <Suspense fallback={<Loader />}>
-                  <GistList gists={gistList} />
-                </Suspense>
-              }
-            </div>
-            <div className="col-8 main-content">
-              <Route exact path="/"  render={() => <div>Welcome to the gist dashboard that shows the gists created in realtime.
-                Please click on a link in sidebar to check them.<span  role="img" aria-label="left-point-emoji">👈 </span></div>} />
-              { gistList && (
-                <Route path="/gist/:gistId" render={({match})=> (
-                  <Suspense fallback={<Loader />}>
-                    <Gist key={match.params.gistId} gist={gistList.find(g=> g.id === match.params.gistId )} />
-                  </Suspense>
-                )} />
+    <Router>
+      <div className="container-fluid">
+        <h1>
+          <Link to="/">Gist Dashboard</Link>
+          <a
+            className="refresh"
+            title="Refresh"
+            onClick={e => handleRefresh(e)}>
+            {!loading && <i className="fa fa-refresh" />}
+            {loading && <Loader />}
+          </a>
+        </h1>
+        {error && (
+          <div className="alert alert-danger" role="alert">
+            error: {error}{' '}
+          </div>
+        )}
+        <div className="row">
+          <div className="col-4 sidebar">
+            {!error && (
+              <Suspense fallback={<Loader />}>
+                <GistList gists={gistList} />
+              </Suspense>
+            )}
+          </div>
+          <div className="col-8 main-content">
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <div>
+                  Welcome to the gist dashboard that shows the gists created in
+                  realtime. Please click on a link in sidebar to check them.
+                  <span role="img" aria-label="left-point-emoji">
+                    👈{' '}
+                  </span>
+                </div>
               )}
-            </div>
+            />
+            {gistList && (
+              <Route
+                path="/gist/:gistId"
+                render={({match}) => (
+                  <Suspense fallback={<Loader />}>
+                    <Gist
+                      key={match.params.gistId}
+                      gist={gistList.find(g => g.id === match.params.gistId)}
+                    />
+                  </Suspense>
+                )}
+              />
+            )}
           </div>
         </div>
-      </Router>
+      </div>
+    </Router>
   );
-
 }
-
-
 export default App;
