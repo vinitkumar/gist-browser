cb4ccc20005c5a108c0d85f74658fc1ac4cb8fe9
Vinit Kumar <mail@vinitkumar.me>
2019-06-05 12:47:44 +0530

    remove not needed files

--- example.tsx
+++
@@ -1,9 +0,0 @@
-export enum ButtonSizes {
-  default = 'default',
-  small = 'small',
-  large = 'large'
-}
-
-export const ButtonPrimary = (
-  props: Props && React.HTMLProps<HTMLButtonElement>
-) => <Button size={ButtonSize.default} {...props}/>;
